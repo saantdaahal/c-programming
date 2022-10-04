06218f31a37666807a57fd909fcc5d0e386c8814
santosh dahal <sirishsds@gmail.com>
2022-10-04 10:33:23 +0545

    C programming basic

---
+++ area.c
@@ -0,0 +1,14 @@
+// Program to find area//
+#include<stdio.h>
+int main(){
+    int length, bregth;
+
+    printf("Enter the length: \n");
+    scanf("%d", &length);
+
+    printf("Enter the bregth: \n");
+    scanf("%d", &bregth);
+
+    printf("The area is: %d", length * bregth);
+    return 0;
+}
---
+++ variables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a = 5+6;
+    float b = 10.11;
+    char c = 'D';
+    int w = 52;
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %f \n", b);
+    printf("The value of a is %c \n", c);
+    printf("The value of a is %d", a+w);
+    return 0;
+}
