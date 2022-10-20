dc1cb9530f597f117fca206a769837c175f13d37
santosh <sirishsds@gmail.com>
2022-10-20 11:13:26 +0545

    File edited by santosh

---
+++ airthmetic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+int a = 4;
+int b = 8;
+// printf("The value of a + b is %d\n", a + b);
+// printf("The value of a - b is %d\n", a - b);
+// printf("The value of a * b is %d\n", a * b);
+// printf("The value of a / b is %d\n", a  b);
+printf("The carry of 5/2 is %d\n", 5%2);
+printf("The value of 4^6 is %f\n",pow(2, 2));
+return 0;
+}
---
+++ instruction.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    float a = 5.2;
+    float b =  a + 4.8;
+    printf("The value of b is %f\n", b);
+    return 0;
+}
